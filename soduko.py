535d5701bd33378e11366c2056166ff9c8f3c304
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-05-23 04:03:36 +0300

    add cross functionality

--- soduko.py
+++ soduko.py
@@ -2,4 +2,11 @@ class Sudoku(object):
 	def __init__(self):
 		self.rows = 'ABCDEFGHI'
 		self.cols = '123456789'
+		self.board = self.cross()
+
+	def cross(self, rows=None, cols=None):
+		rows = rows or self.rows
+		cols = cols or self.cols
+		return [r + c for r in rows for c in cols]
+
 
