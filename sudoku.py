14df9485139eba2d57b42f1ff31a4b883e570709
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-05-23 04:45:00 +0300

    add elimination

--- sudoku.py
+++ sudoku.py
@@ -4,6 +4,8 @@ class Sudoku(object):
 		self.cols = '123456789'
 		self.board = self.cross()
 		self.unit_list = self.row_units() + self.col_units() + self.square_units()
+		self.units = dict((s, [u for u in self.unit_list if s in u]) for s in self.board)
+		self.peers = dict((s, set(sum(self.units[s], [])) - set([s])) for s in self.board)
 
 	def cross(self, rows=None, cols=None):
 		rows = rows or self.rows
@@ -24,8 +26,9 @@ class Sudoku(object):
 
 	def grid_values(self, values):
 		"""Convert grid string into {<box>: <value>} dict with '.' value for empties."""
+		all_digits = '123456789'
 		assert len(values) == 9 * 9, 'Input grid must be of size 9x9'
-		return dict(zip(self.board, values))
+		return {box: all_digits if value == '.' else value for box, value in zip(self.board, values)}
 
 	def display_board(self, values):
 		"""
@@ -40,6 +43,14 @@ class Sudoku(object):
 				print(line)
 		return
 
+	def eliminate(self, values):
+		solved_boxes = [box for box in values.keys() if len(values.get(box)) == 1]
+		for box in solved_boxes:
+			digit = values[box]
+			for peer in self.peers[box]:
+				values[peer] = values[peer].replace(digit, '')
+		return values
+
 	def __str__(self):
 		return "{}".format(self.board)
 
@@ -47,4 +58,8 @@ class Sudoku(object):
 sudoku = Sudoku()
 unsolved = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
 
+print('BEFORE ELIMINATION')
 sudoku.display_board(sudoku.grid_values(unsolved))
+
+print('AFTER ELIMINATION')
+sudoku.display_board(sudoku.eliminate(sudoku.grid_values(unsolved)))
