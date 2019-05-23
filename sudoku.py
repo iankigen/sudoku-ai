4442a95b46a43c958bff56ba7c6dba8db90d7fb2
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-05-23 04:05:35 +0300

    add grid_values and display_board functions

--- sudoku.py
+++ sudoku.py
@@ -22,4 +22,29 @@ class Sudoku(object):
 		all_square_units = [self.cross(rows, cols) for rows in ('ABC', 'DEF', 'GHI') for cols in ('123', '456', '789')]
 		return all_square_units[position] if 0 <= position < 9 else all_square_units
 
+	def grid_values(self, values):
+		"""Convert grid string into {<box>: <value>} dict with '.' value for empties."""
+		assert len(values) == 9 * 9, 'Input grid must be of size 9x9'
+		return dict(zip(self.board, values))
 
+	def display_board(self, values):
+		"""
+		Display the values as a 2-D grid.
+		"""
+		width = 1 + max(len(values[s]) for s in self.board)
+		line = '+'.join(['-' * (width * 3)] * 3)
+		for row in self.rows:
+			print(''.join(values[row + col].center(width) + ('|' if col in '36' else '') for col in self.cols))
+
+			if row in 'CF':
+				print(line)
+		return
+
+	def __str__(self):
+		return "{}".format(self.board)
+
+
+sudoku = Sudoku()
+unsolved = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
+
+sudoku.display_board(sudoku.grid_values(unsolved))
